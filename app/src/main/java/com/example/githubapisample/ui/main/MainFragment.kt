9f2defc9edd052a17d886a8f4902bc9ec3cb97d5
numberH2O <109640143+numberH2O@users.noreply.github.com>
2025-03-22 16:26:15 +0900

    listView追加

--- app/src/main/java/com/example/githubapisample/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/githubapisample/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import com.example.githubapisample.R
 
 class MainFragment : Fragment() {
@@ -16,17 +17,26 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    private lateinit var repositoryNameAdapter: RepositoryNameAdapter
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        repositoryNameAdapter = RepositoryNameAdapter(
+            context = context!!,
+            resource = R.layout.list_item,
+            items = emptyList(),
+        )
+
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        view.findViewById<ListView>(R.id.listview).adapter = repositoryNameAdapter
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
-
 }
